f897ef9ff4320aba1448fcfa973fa27a8c99b5e7
liamradley1 <liamradley1@gmail.com>
2020-05-16 21:36:03 +0100

    General functionality of RSA now present
    Just need to add PKXS1_OAEP funcitionality

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RSA Algorithm
- Basic implementation of the RSA Algorithm in Python
+- Basic implementation of the RSA Algorithm in Python.
+- Generates keys using subroutine in Python's RSA library.
+-
--- rsa.py
+++ rsa.py
@@ -4,39 +4,38 @@ Created on Fri May 15 14:05:26 2020
 
 @author: Liam
 """
-
-import os
+import string
+import time
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP as P
+import os
 
 
-def generateKeysInHouse(): # Uses the RSA library to create a private key. Note public key is included as a subset of this. 
-    key=RSA.generate(1024)
-    return key
+def generateKeys():                                         # Uses the RSA library to create private keys.
+    key=RSA.generate(1024)                                  # Note public key is included as a subset of this key. 
+    return key                                              # Unit test omitted as this only invokes a library subroutine.
+                                                            # Any cipher could be used - using PKCS1OAEP here.
 
-def generateKeysOSSL():
-    os.system('cmd /c "openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:1028 & openssl rsa -pubout -in private_key.pem & openssl rsa -text -in private_key.pem')
-    f = open('private_key.pem', 'rb')
-    key=RSA.importKey(f.read())
-    return key
 
-def padText(plainText):                                     # Only converts plaintext into ASCII notation. 
-                                                            # Need to add PKCS1_OAEP padding.
-    paddedPlainText=[ord(c) for c in plainText]
+def getPublicKey(key):
+    return key.publickey()
+
+def padText(plainText, publicKey):                  # Only converts plaintext into ASCII notation. 
+    paddedPlainText=[ord(c) for c in plainText]             # Can use any padding cipher.
     return paddedPlainText
+    
 
-def unpadText(paddedText):
+def unpadText(paddedText, publicKey):                       # Will unit test as a pair in order to ensure correct padding.
     plainText=[chr(c) for c in paddedText]
     return plainText
 
-def rsaEncrypt(keys,padPlain):                              # Encrypts using the RSA algorithm - runs in O(log(n))
-                                                            # due to built-in modular exponentation function.
-
-    padEncrypt=[pow(m,keys.e,keys.n) for m in padPlain]
+def rsaEncrypt(keys, padPlain):                             # Encrypts using the RSA algorithm - runs in O(log(n))
+    padEncrypt=[pow(m, keys.e, keys.n) for m in padPlain]   # due to built-in modular exponentation function.
     return padEncrypt
+    
 
-def rsaDecrypt(keys,padEncrypt):                           
-    padDecrypt=[pow(c,keys.d,keys.n) for c in padEncrypt]
+def rsaDecrypt(keys, padEncrypt):                           
+    padDecrypt=[pow(c, keys.d, keys.n) for c in padEncrypt]
 
     return padDecrypt
 
@@ -44,20 +43,61 @@ def exampleKey():                                           # Provides small tes
     e=5
     p=13
     q=17
-    n=13*17
+    n=13 * 17
     d=77
-    components=(n,e,d,p,q)
+    components=(n, e, d, p, q)
     key=RSA.construct(components)
     return key
 
+def encryptAndSend(plainText, file, key):                   
+    publicKey=getPublicKey(key)                             
+    padPlain=padText(plainText, publicKey)                   
+    padEncrypt=rsaEncrypt(key, padPlain)                    
+    padEncrypt=str(padEncrypt)
+    f=open(file, "w")
+    f.write(padEncrypt)
+
+def readAndDecrypt(file, key):
+    publicKey=getPublicKey(key)
+    f=open(file, "r")
+    padEncrypt = list(f.read())
+    padEncrypt2 = []
+    elt2 = ""
+    for elt in padEncrypt:
+        if(elt.isdigit() == True):
+            elt2=elt2 + str(elt)
+        else:
+            if(elt2 != ""):
+                padEncrypt2.append(int(elt2))
+                elt2 = ""
+    padEncrypt = padEncrypt2
+    padDecrypt = rsaDecrypt(key, padEncrypt)
+    stringText = unpadText(padDecrypt, publicKey)
+    plainText = ""
+    for elt in stringText:
+        plainText = plainText + elt
+    return plainText
+
+def rsaTest():
+    keys=generateKeys()
+    file="message.txt"
+    plainText=string.ascii_letters+"0123456789"
+    createMessage(plainText, keys)
+    encryptAndSend(plainText, file, keys)
+    message = readAndDecrypt(file, keys)
+    assert(message == plainText)
+
+
+def createMessage(plainText, keys):
+    file = "message.txt"
+    encryptAndSend(plainText, file, keys)
+
 def main():
-    keys=generateKeysInHouse()
-    plainText="Hello world!"
-    padPlain=padText(plainText)
-    print(padPlain)
-    padEncrypt=rsaEncrypt(keys,padPlain)  
-    padDecrypt=rsaDecrypt(keys,padEncrypt)
-    plainText=unpadText(padDecrypt)
-    print(plainText)
+    #rsaTest()
+    keys=generateKeys()
+    message= "Hello, World!"
+    file="message.txt"
+    createMessage(message, keys)
+    print(readAndDecrypt(file, keys))
 
 main()
